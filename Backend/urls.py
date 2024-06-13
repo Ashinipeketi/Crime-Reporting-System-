b86ecea5179b767fd7142f9d25e56fe03210565f
Ashinipeketi <162983910+Ashinipeketi@users.noreply.github.com>
2024-06-13 22:42:37 +0530

    Update urls.py

--- Backend/urls.py
+++ Backend/urls.py
@@ -1,1 +1,20 @@
+# crimeapp/urls.py
+
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    # path('signout', views.signout),
+    path('report', views.report, name='report'),
+    path('fetch', views.fetch, name="fetch"),
+    path('delete/<str:username>/', views.delete, name="delete"),
+    path('update/<int:case_id>/', views.update, name="update"),
+    # Police URLs
+    path('police/register/', views.police_register, name='police_register'),
+    path('police/login/', views.police_login, name='police_login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+]
 
