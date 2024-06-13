b7508faf1ea5896504ba4aeda1f3301aef038759
Ashinipeketi <162983910+Ashinipeketi@users.noreply.github.com>
2024-06-13 22:52:52 +0530

    Update message.py

--- Backend/message.py
+++ Backend/message.py
@@ -1,1 +1,11 @@
+from twilio.rest import Client
+from django.conf import settings
+
+def send_sms(to, message):
+    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+    client.messages.create(
+        to="8639XXXXXX",
+        from_=settings.TWILIO_PHONE_NUMBER,
+        body="HI HELLO WORLD"
+    )
 
